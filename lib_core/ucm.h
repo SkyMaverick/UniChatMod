dc01037689d8f07a1b417742d75090bb467b73b4
SkyMaverick <skymaverickas@gmail.com>
2018-08-04 13:35:57 +0300

    core: redefine message structure

--- lib_core/ucm.h
+++ lib_core/ucm.h
@@ -123,15 +123,23 @@ enum {
     UCM_FLAG_MSG_ALERT      = 1 << 2
 };
 
+enum {
+    UCM_TYPE_MSG_NORMAL  = 0,
+    UCM_TYPE_MSG_FILE    = 1,
+    UCM_TYPE_MSG_ROOM    = 2,
+    UCM_TYPE_MSG_STATUS  = 3,
+    UCM_TYPE_MSG_EVENT   = 4
+};
+
 typedef struct ucm_msg_s {
-    uint32_t    flags;          // message processing flags
-    uint32_t    uid;            // uid string offset
-    uint32_t    ip;             // sender ip
-    time_t      time;           // time send msg
-    uint32_t    theme;          // 
-    // message body area
-    uint32_t    size;           // size data field
-    char        data [1];       // strings storage
+    uint8_t     type;
+    // TODO sender info
+    time_t      time;
+    uint32_t    flags;
+    struct {
+        size_t  size;
+        uint8_t blob [1];
+    } data;
 } ucm_message_t;
 
 // ######################################################################
