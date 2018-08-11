7ebe117f2d624f2311fdc648e2a1f66fa92301c0
SkyMaverick <skymaverickas@gmail.com>
2018-08-11 19:14:47 +0300

    unicode: add some unicode string operations

--- lib_core/utf8proc/utf8.c
+++ lib_core/utf8proc/utf8.c
@@ -7,19 +7,21 @@
 #define CHAR_SIZE sizeof(uchar_t)
 
 static int
-uc_valid (uchar_t* str)
+uc_valid (uchar_t* str,
+          ssize_t   lenght)
 {
-    UNUSED (str);
-    return 0;
+    while (lenght >= 0)
+        if ( !utf8proc_codepoint_valid (str[ lenght-- ]) )
+            return 0;
+    return 1;
 }
 
 static size_t
 uec_strlen (uechar_t* str)
 {
-    size_t  n, count = 0;
     uchar_t code;
-    while ((n = utf8proc_iterate(str, -1, &code)) > 0)
-        count++;
+    size_t  count = 0;
+    while ( utf8proc_iterate ( str, -1, &code ) > 0) count++;
     return count;
 }
 
@@ -38,7 +40,7 @@ uec2str (uechar_t** ue_str)
                                                                |UTF8PROC_COMPOSE
                                                                |UTF8PROC_STABLE) > 0 ) 
         {
-            if (!ucm_realloc ((void**)ue_str, buf_len)) {
+            if ( !ucm_realloc ((void**)ue_str, buf_len) ) {
                 memcpy (*ue_str, buf, buf_len);
                 ucm_free (buf);
                 return ((ucm_string_t*)(*ue_str))->lenght;
@@ -63,4 +65,90 @@ str2uec (ucm_string_t** u_str)
         return ( realloc (*u_str, bytes) ) ? -1 : bytes;
     }
     return -1;
+}
+
+// ===================================
+
+size_t 
+ucm_strlen (ucm_string_t* string)
+{
+    return string->lenght - 1;
+}
+
+ucm_string_t*
+ucm_strdup (ucm_string_t* string)
+{
+    size_t ret_size = sizeof(ucm_string_t)          // struct size
+                      + string->lenght * CHAR_SIZE; // string lenght
+    ucm_string_t* ret = ucm_malloc ( ret_size );
+    if (ret)
+        memcpy (ret, string, ret_size );
+    return ret;
+}
+
+void
+ucm_strcat (ucm_string_t* result,
+            ucm_string_t** cat)
+{
+    size_t new_size = sizeof(ucm_string_t)          // struct size
+                      + result->lenght * CHAR_SIZE  // 1-st string lenght
+                      + (*cat)->lenght * CHAR_SIZE  // 2-nd string lenght
+                      - CHAR_SIZE;                  // - one /0 terminate
+    if ( !ucm_realloc ( (void*)(&result), new_size) ) {
+        memcpy ( &( result->data [ result->lenght - 1 ] ), (*cat)->data, (*cat)->lenght);
+        result->lenght = new_size;
+        ucm_free_null (*cat);
+    }
+}
+
+int8_t
+ucm_strcmp (ucm_string_t* left,
+            ucm_string_t* right)
+{
+    if (left->lenght != right->lenght)
+        return ( left->lenght > right->lenght ) ? -1 : 1;
+    for (size_t i=0; i < left->lenght; i++)
+        if ( left->data[ i ] != right->data[ i ] )
+            return ( left->data[ i ] > right->data[ i ] ) ? -1 : 1;
+    return 0;
+}
+
+int8_t
+ucm_strcasecmp (ucm_string_t* left,
+                ucm_string_t* right)
+{
+    if (left->lenght != right->lenght)
+        return ( left->lenght > right->lenght ) ? -1 : 1;
+    for (size_t i=0; i < left->lenght; i++) 
+    {
+        uchar_t a = utf8proc_tolower ( left->data[ i ] );
+        uchar_t b = utf8proc_tolower ( right->data[ i ] );
+
+        if ( a != b )
+            return ( a > b ) ? -1 : 1;
+    }
+    return 0;
+}
+
+int8_t
+ucm_strncmp (ucm_string_t* left,
+             ucm_string_t* right,
+             size_t        num)
+{
+
+}
+
+void
+ucm_strcpy (ucm_string_t* dest,
+            ucm_string_t* source)
+{
+
+}
+
+void
+ucm_strncpy (ucm_string_t* dest,
+             ucm_string_t* source,
+             size_t        num)
+{
+
 }
--- lib_core/utf8proc/utf8.h
+++ lib_core/utf8proc/utf8.h
@@ -15,17 +15,36 @@ typedef struct {
 } ucm_string_t;
 #define RAWSTR(X) (X).data
 
+size_t 
+ucm_strlen (ucm_string_t* string);
 
+ucm_string_t*
+ucm_strdup (ucm_string_t* string);
 
-// /*
-//  TODO
-//  strlen,
-//  strcmp,
-//  strchr,
-//  strcpy,
-//  strdup,
-//  str2buf,
-//  <S-F12>
-//  */
+void
+ucm_strcat (ucm_string_t*  result,
+            ucm_string_t** cat);
+
+int8_t
+ucm_strcmp (ucm_string_t* left,
+            ucm_string_t* right);
+
+int8_t
+ucm_strcasecmp (ucm_string_t* left,
+                ucm_string_t* right);
+
+int8_t
+ucm_strncmp (ucm_string_t* left,
+             ucm_string_t* right,
+             size_t        num);
+
+void
+ucm_strcpy (ucm_string_t* dest,
+            ucm_string_t* source);
+
+void
+ucm_strncpy (ucm_string_t* dest,
+             ucm_string_t* source,
+             size_t        num);
 
 #endif
