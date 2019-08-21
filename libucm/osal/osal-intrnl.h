d5e9d82fef6f9d6d0a921182e1ac2e5e652575dd
SkyMaverick <skymaverickas@gmail.com>
2019-08-21 20:26:52 +0300

    osal: remove osal-system.[c,h]. Move this into osal-intrnl.[c,h]

--- libucm/osal/osal-intrnl.h
+++ libucm/osal/osal-intrnl.h
@@ -5,9 +5,13 @@
 #include "osal-dynlib.h"
 #include "osal-fs.h"
 #include "osal-memory.h"
-#include "osal-system.h"
 #include "osal-threading.h"
 #include "osal-timers.h"
+
+enum {
+    OSAL_LOOP_SYSTEM  = 0,
+    OSAL_LOOP_NETWORK = 1
+};
 
 typedef struct {
     uv_loop_t* loop_system;
@@ -15,6 +19,16 @@ typedef struct {
 } osal_handler_t;
 
 extern osal_handler_t* o_krnl;
+
+int
+osal_run (int         loop,
+          uv_run_mode mode);
+int
+osal_errno (void);
+uintptr_t
+osal_init (void);
+int
+osal_release (void);
 
 #define LOOP_KRNL   o_krnl->loop_system
 #define LOOP_NET    o_krnl->loop_network
