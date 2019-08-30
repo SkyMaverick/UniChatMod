47144e51cb490048ac6aa4f7d0cf67ce202c47e7
SkyMaverick <skymaverickas@gmail.com>
2019-08-30 21:46:13 +0300

    osal: remove libuv wrappers. Move UV code in core library. UV library provide libuv API in plugins as part main API

--- libucm/osal/osal-intrnl.h
+++ libucm/osal/osal-intrnl.h
@@ -5,22 +5,3 @@
 #include "osal-fs.h"
 #include "osal-memory.h"
 #include "osal-threading.h"
-#include "osal-timers.h"
-#include "osal-uvwrap.h"
-
-enum { OSAL_LOOP_SYSTEM = 0, OSAL_LOOP_NETWORK = 1 };
-
-typedef struct {
-    uv_loop_t* loop_system;
-    uv_loop_t* loop_network;
-} osal_handler_t;
-
-extern osal_handler_t* o_krnl;
-
-int osal_run(int loop, uv_run_mode mode);
-int osal_errno(void);
-uintptr_t osal_init(void);
-int osal_release(void);
-
-#define LOOP_KRNL o_krnl->loop_system
-#define LOOP_NET o_krnl->loop_network
